1e841ab95c3cfc7baf4f764965b397106ffd6ba8
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2022-09-07 01:52:17 +0900

    Update dot_config/nvim/lua/term.lua

--- dot_config/nvim/lua/term.lua
+++ dot_config/nvim/lua/term.lua
@@ -1,2 +1,5 @@
 vim.api.nvim_create_user_command("T", "split | wincmd j | resize 20 | terminal <args>", {})
-vim.api.nvim_set_keymap("t", "<ESC>", "<C-\\><C-n>", {})
+vim.api.nvim_set_keymap("t", "<C-q>", "<C-\\><C-n>", {})
+vim.cmd [[
+    autocmd TermOpen * startinsert
+]]
