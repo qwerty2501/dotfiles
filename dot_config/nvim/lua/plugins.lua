6de74419411768d0e360567f1e226559e0b1ea47
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2022-09-07 13:55:50 +0900

    Update dot_config/nvim/lua/plugins.lua Update dot_config/nvim/plugin/packer_compiled.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 local packer_bootstrap = ensure_packer()
 vim.cmd([[
@@ -17,29 +17,38 @@ vim.cmd([[
 ]])
 
 return require("packer").startup(function(use)
-    use "wbthomason/packer.nvim"
-    use "rstacruz/vim-closer"
-    use "williamboman/mason-lspconfig.nvim"
-    use "neovim/nvim-lspconfig"
-    use "williamboman/mason.nvim"
-    use "mfussenegger/nvim-dap"
-    use "jose-elias-alvarez/null-ls.nvim"
-    use "hrsh7th/nvim-cmp"
-    use "hrsh7th/cmp-nvim-lsp"
-    use "hrsh7th/vim-vsnip"
-    use "tpope/vim-fugitive"
-    use "ibhagwan/fzf-lua"
-    use {
-        "kyazdani42/nvim-tree.lua",
-        requires = {
-            "kyazdani42/nvim-web-devicons",
-        },
-    }
-    use "APZelos/blamer.nvim"
-    use "simrat39/rust-tools.nvim"
-    use "buoto/gotests-vim"
-
-    if packer_bootstrap then
-        require("packer").sync()
+  use "wbthomason/packer.nvim"
+  use "rstacruz/vim-closer"
+  use "williamboman/mason-lspconfig.nvim"
+  use "neovim/nvim-lspconfig"
+  use "williamboman/mason.nvim"
+  use "mfussenegger/nvim-dap"
+  use "jose-elias-alvarez/null-ls.nvim"
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/vim-vsnip"
+  use "tpope/vim-fugitive"
+  use "ibhagwan/fzf-lua"
+  use {
+    "kyazdani42/nvim-tree.lua",
+    requires = {
+      "kyazdani42/nvim-web-devicons",
+    },
+  }
+  use "APZelos/blamer.nvim"
+  use "simrat39/rust-tools.nvim"
+  use {
+    "klen/nvim-test",
+    requires = {
+      "nvim-treesitter/nvim-treesitter",
+      run = function() require('nvim-treesitter.install').update({ with_sync = true }) end
+    },
+    config = function()
+      require('nvim-test').setup()
     end
+  }
+
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
--- dot_config/nvim/plugin/packer_compiled.lua
+++ dot_config/nvim/plugin/packer_compiled.lua
@@ -89,11 +89,6 @@ _G.packer_plugins = {
     path = "/home/oikawa-k/.local/share/nvim/site/pack/packer/start/fzf-lua",
     url = "https://github.com/ibhagwan/fzf-lua"
   },
-  ["gotests-vim"] = {
-    loaded = true,
-    path = "/home/oikawa-k/.local/share/nvim/site/pack/packer/start/gotests-vim",
-    url = "https://github.com/buoto/gotests-vim"
-  },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
     path = "/home/oikawa-k/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
@@ -124,10 +119,21 @@ _G.packer_plugins = {
     path = "/home/oikawa-k/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
+  ["nvim-test"] = {
+    config = { "\27LJ\2\0027\0\0\2\0\3\0\0066\0\0\0'\1\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14nvim-test\frequire\0" },
+    loaded = true,
+    path = "/home/oikawa-k/.local/share/nvim/site/pack/packer/start/nvim-test",
+    url = "https://github.com/klen/nvim-test"
+  },
   ["nvim-tree.lua"] = {
     loaded = true,
     path = "/home/oikawa-k/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/kyazdani42/nvim-tree.lua"
+  },
+  ["nvim-treesitter"] = {
+    loaded = true,
+    path = "/home/oikawa-k/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
@@ -162,6 +168,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: nvim-test
+time([[Config for nvim-test]], true)
+try_loadstring("\27LJ\2\0027\0\0\2\0\3\0\0066\0\0\0'\1\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14nvim-test\frequire\0", "config", "nvim-test")
+time([[Config for nvim-test]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
