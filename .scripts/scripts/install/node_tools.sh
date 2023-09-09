d7a45e443252d5d04a2373448cad944b73a7480a
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2023-09-09 20:17:09 +0900

    Update .scripts/configs/install/node_tools Update .scripts/scripts/install/node_tools.sh

--- .scripts/scripts/install/node_tools.sh
+++ .scripts/scripts/install/node_tools.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -eu
 
+sudo npm i -g npm
+sudo npm i -g n
+sudo n latest
+
 cat $(dirname $0)/../../configs/install/node_tools | while read line
 do
 	sudo npm i -g $line
 done
-sudo n latest
