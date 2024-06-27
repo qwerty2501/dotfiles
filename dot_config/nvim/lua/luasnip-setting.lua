ddcb1e95e039c0e32bd9ac6ff7d202d511757ed1
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2024-06-27 16:43:24 +0900

    Update .config/nvim/lua/luasnip-setting.lua

--- dot_config/nvim/lua/luasnip-setting.lua
+++ dot_config/nvim/lua/luasnip-setting.lua
@@ -1,2 +1,3 @@
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_vscode").lazy_load({ paths = "./my_snippets" })
+require("luasnip.loaders.from_vscode").lazy_load({ paths = vim.fn.getcwd() .. "/.my_snippets" })
