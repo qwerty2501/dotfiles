c1804a6acda9dc28d234d64e8f71dab5a5a44a03
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-07-18 23:57:56 +0900

    Update run_once_init.sh
    debug

--- run_once_init.sh
+++ run_once_init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu
-$CHEZMOI_DIR=$($HOME/bin/chezmoi source-path)
+CHEZMOI_DIR=$($HOME/bin/chezmoi source-path)
 echo "chezmoi dir $$CHEZMOI_DIR"
 $CHEZMOI_DIR/.scripts/init.sh
 exit 0
