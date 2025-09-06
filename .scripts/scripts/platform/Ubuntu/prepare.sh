5585a72ac3caadac926a002c8060802bef1b8234
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-09-07 08:33:45 +0900

    feat: move base_apt to prepare

--- .scripts/scripts/platform/Ubuntu/prepare.sh
+++ .scripts/scripts/platform/Ubuntu/prepare.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt install -y build-essential
+sudo apt install -y apt-transport-https \
+	ca-certificates \
+	gnupg-agent \
+	software-properties-common \
+	curl \
+	wget \
+  build-essential \
+	git
+
+sudo install -m 0755 -d /etc/apt/keyrings
+
+sudo apt update
