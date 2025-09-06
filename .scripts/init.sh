4c026c6b89f39046698732717092bcd423cd3298
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-09-07 08:21:13 +0900

    feat: use case wildcard

--- .scripts/init.sh
+++ .scripts/init.sh
@@ -2,9 +2,10 @@
 set -eu
 
 
-if [ "$(uname -a | grep Ubuntu)" != "" ]; then
-  export TARGET_PLATFORM="Ubuntu"
-fi
+case "$(uname -a)" in
+  *Ubuntu* ) export TARGET_PLATFORM="Ubuntu" ;;
+esac
+
 
 CHEZMOI_DIR=$HOME/.local/share/chezmoi
 "$CHEZMOI_DIR/.scripts/scripts/platform/$TARGET_PLATFORM/prepare.sh"
