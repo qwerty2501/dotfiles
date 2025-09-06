aab394ada729ce37460a1ca48cc0b85f9065230b
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-09-07 08:25:48 +0900

    feat: add no-modify-path

--- .scripts/scripts/general/install/rust.sh
+++ .scripts/scripts/general/install/rust.sh
@@ -2,5 +2,5 @@
 set -eu
 
 if [ ! "$(command -v rustc)" ]; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- --no-modify-path
 fi
