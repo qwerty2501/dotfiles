29cfa4c2d95d37e55bd400184e090b4e45efef54
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-09-07 16:12:18 +0900

    fix: rustup parameters

--- .scripts/scripts/general/install/rust.sh
+++ .scripts/scripts/general/install/rust.sh
@@ -2,5 +2,5 @@
 set -eu
 
 if [ ! "$(command -v rustc)" ]; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- --no-modify-path
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
 fi
